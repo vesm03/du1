a9591eb249d8e1ab102972d7c8169d0a74a725a2
Vesy <Vesy@VESY-NTB>
2018-04-04 10:46:12 +0200

    uvodni commit prazdneho projektu

---
+++ du1/src/main/java/com/github/vesm03/du1/App.java
@@ -0,0 +1,13 @@
+package com.github.vesm03.du1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
